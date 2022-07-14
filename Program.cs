8174e6daee615bd45f3d8703be6de3b5915cd1e0
Giznulf <90238505+Giznulf@users.noreply.github.com>
2022-07-14 09:12:08 +0300

    Добавил две реализации процесса сортировки. Стандартным пузырьком и кортежем.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Sorting
 {
@@ -7,91 +8,47 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            Program program = new Program();
+            Program sorting = new Program();
             while (true)
             {
-                program.Output();
+                sorting.Output();
             }
         }
         public string Input()
         {
             Console.Write("Введите числа через пробел: ");
-            string input = Console.ReadLine();
-            return input;
-        }
-        public void Parse(out int[] numbers, out bool hasNumber, out string filterString)
+            return Console.ReadLine();
+        } 
+        public void ValidationAndConversion(string inputString,out double[] numbers)  //Валидация и конвертация
         {
-            string inputStrig = Input();
+            if(Regex.IsMatch(inputString, @"(-?[0-9]+(\.{1}[0-9]+)?){1,}"))  //Проверка на числа
+            {
+                string[] parseString = inputString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                numbers = parseString.Select(x => double.Parse(x)).ToArray();
+            }
+            else numbers = new double[0];                
+        }
+
+        public double[] Sorting(double[] numbers) => numbers.OrderBy(n => n).ToArray();  //Сортировка
+
+        public void Output()
+        {
+            SortingImplementation sorting = new SortingImplementation();
             try
             {
-                string[] parseString = inputStrig.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                filterString = inputStrig.Replace(" ", "");
-                string pattern = @"[0-9]";
-                hasNumber = Regex.IsMatch(filterString, pattern);
-                if (hasNumber)
-                {
-                    numbers = Array.ConvertAll(parseString, new Converter<string, int>(Converter));
-                }
-                else
-                {
-                    numbers = new int[0];
-                }
+                string inputStrig = Input();                           //Получение вводимой строки
+                ValidationAndConversion(inputStrig, out double[] unsortedNumbers);   //Проверка на корректность и получение ассива чисел
+
+                Console.WriteLine($"Сортировка корежем {string.Join(" ", sorting.Tuple(unsortedNumbers))}");
+
+                Console.WriteLine($"Сортировка пузырьком {string.Join(" ", sorting.Bubble(unsortedNumbers))}");
+
+                Console.WriteLine($"Сортировка LINQ {string.Join(" ", Sorting(unsortedNumbers))}"); 
             }
             catch
             {
-                numbers = new int[0];
-                hasNumber = false;
-                filterString = "";
-            }
-
-            int Converter(string i)
-            {
-                int number = int.Parse(i);
-                return number;
-            }
-        }
-        public void Sort(out string stringNumbers, out bool hasNumber, out string exitProgram)
-        {
-            Parse(out int[] numbers, out hasNumber, out exitProgram);
-            if (hasNumber)
-            {                
-                for (int i = 0; i < numbers.Length - 1; i++)
-                {
-                    for (int j = i + 1; j < numbers.Length; j++)
-                    {
-                        if (numbers[i] > numbers[j])
-                        {
-                            int x;
-
-                            x = numbers[i];
-                            numbers[i] = numbers[j];
-                            numbers[j] = x;
-                        }
-                    }
-                }
-                stringNumbers = string.Join(" ", numbers);
-            }
-            else
-            {
-                stringNumbers = "";
-            }
-        }
-        public void Output()
-        {
-            Sort(out string outputMessage, out bool hasNumber, out string exitProgram);
-            if (hasNumber)
-            {
-                Console.WriteLine(outputMessage);
-            }
-            else if (exitProgram == "Выход")
-            {
-                Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine("Не все введеные значения являються числам, попробуйте еще раз.");
-            }
-            Console.WriteLine("Введите 'Выход' чтобы завершить.\n");
+                Console.WriteLine("Не все введеные значения являються числам, попробуйте еще раз."); //Исключение в случае ошибки
+            }            
         }
     }
 }
---
+++ SortingImplementation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    internal class SortingImplementation
+    {
+        public double[] Tuple(double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                        (numbers[i], numbers[j]) = (numbers[j], numbers[i]);  //Меняем кортежем местами числа
+                }
+            }
+            return numbers;
+        }
+        public double[] Bubble(double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    double temporary;
+
+                    if (numbers[i] > numbers[j])
+                    {
+                        temporary = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temporary;
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
