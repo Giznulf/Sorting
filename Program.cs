8e0701569a6637cc53525fdb7fa8d324bcaaca5e
Giznulf <90238505+Giznulf@users.noreply.github.com>
2022-07-14 18:49:42 +0300

    Добавил два вартианта сортировки, кортежем и пузырькомОба выводятся на консоль с пояснениями.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Linq;
@@ -52,3 +53,54 @@ namespace Sorting
         }
     }
 }
+=======
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program sorting = new Program();
+            while (true)
+            {
+                sorting.Output();
+            }
+        }
+        public string Input()
+        {
+            Console.Write("Введите числа через пробел: ");
+            return Console.ReadLine();
+        } 
+        public void ValidationAndConversion(string inputString,out double[] numbers)  //Валидация и конвертация
+        {
+            if(Regex.IsMatch(inputString, @"(-?[0-9]+(\.{1}[0-9]+)?){1,}"))  //Проверка на числа
+            {
+                string[] parseString = inputString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                numbers = parseString.Select(x => double.Parse(x)).ToArray();
+            }
+            else numbers = new double[0];                
+        }
+
+        public double[] Sort(double[] numbers) => numbers.OrderBy(n => n).ToArray();  //Сортировка
+
+        public void Output()
+        {
+            try
+            {
+                string inputStrig = Input();                           //Получение вводимой строки
+                ValidationAndConversion(inputStrig, out double[] unsortedNumbers);   //Проверка на корректность и получение ассива чисел
+                double[] sortedNumbers = Sort(unsortedNumbers);      //Сортировка
+                Console.WriteLine(string.Join(" ", sortedNumbers)); //Вывод сортированной строки
+            }
+            catch
+            {
+                Console.WriteLine("Не все введеные значения являються числам, попробуйте еще раз."); //Исключение в случае ошибки
+            }            
+        }
+    }
+}
+>>>>>>> 26cd6acc6318e0521b1f48836dfbcdf636821fc1
